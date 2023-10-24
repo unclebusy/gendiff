38194e2ec99f76311a444d4bea2d68a245246f73
Denis Kalagin <deniskalagin@gmail.com>
2023-10-24 18:51:12 +0600

    add eslint & update makefile

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,16 +1,16 @@
 #!/usr/bin/env node
 
 import { program } from 'commander';
-import makeParsingData from "../src/MakeParsingData.js";
+import makeParsingData from '../src/MakeParsingData.js';
 
 program
-	.name('gendiff')
-	.description('Compares two configuration files and shows a difference.')
-	.version('1.0.0')
-	.arguments('<filepath1> <filepath2>')
-	.option('-f, --format <type>', 'output format', 'stylish')
-	.usage('Usage: gendiff [options] <filepath1> <filepath2>')
-	.action((filepath1, filepath2) => {
-		console.log(makeParsingData(filepath1, filepath2))
-	})
-	.parse(process.argv);
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .version('1.0.0')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format <type>', 'output format', 'stylish')
+  .usage('Usage: gendiff [options] <filepath1> <filepath2>')
+  .action((filepath1, filepath2) => {
+    console.log(makeParsingData(filepath1, filepath2));
+  })
+  .parse(process.argv);
--- src/MakeParsingData.js
+++ src/MakeParsingData.js
@@ -3,33 +3,32 @@ import fs from 'fs';
 import _ from 'lodash';
 
 const compareObjects = (obj1, obj2) => {
-    const keys = _.sortBy(_.union(Object.keys(obj1), Object.keys(obj2)));
+  const keys = _.sortBy(_.union(Object.keys(obj1), Object.keys(obj2)));
 
-    const result =  keys.map((key) => {
-        if (!_.has(obj2, key)) {
-            return `  - ${key}: ${obj1[key]}`;
-        }
-        if (!_.has(obj1, key)) {
-            return `  + ${key}: ${obj2[key]}`;
-        }
-        if (obj1[key] !== obj2[key]) {
-            return `  - ${key}: ${obj1[key]}\n  + ${key}: ${obj2[key]}`;
-        }
-        return `    ${key}: ${obj1[key]}`;
-    }).join('\n');
+  const result = keys.map((key) => {
+    if (!_.has(obj2, key)) {
+      return `  - ${key}: ${obj1[key]}`;
+    }
+    if (!_.has(obj1, key)) {
+      return `  + ${key}: ${obj2[key]}`;
+    }
+    if (obj1[key] !== obj2[key]) {
+      return `  - ${key}: ${obj1[key]}\n  + ${key}: ${obj2[key]}`;
+    }
+    return `    ${key}: ${obj1[key]}`;
+  }).join('\n');
 
-    return `{\n${result}\n}`;
+  return `{\n${result}\n}`;
 };
 
 const makeParsingData = (filepath1, filepath2) => {
-    const absolutePath1 = path.resolve(process.cwd(), filepath1);
-    const absolutePath2 = path.resolve(process.cwd(), filepath2);
+  const absolutePath1 = path.resolve(process.cwd(), filepath1);
+  const absolutePath2 = path.resolve(process.cwd(), filepath2);
 
-    const data1 = JSON.parse(fs.readFileSync(absolutePath1, 'utf-8'));
-    const data2 = JSON.parse(fs.readFileSync(absolutePath2, 'utf-8'));
+  const data1 = JSON.parse(fs.readFileSync(absolutePath1, 'utf-8'));
+  const data2 = JSON.parse(fs.readFileSync(absolutePath2, 'utf-8'));
 
-    return compareObjects(data1, data2);
-
+  return compareObjects(data1, data2);
 };
 
 export default makeParsingData;
