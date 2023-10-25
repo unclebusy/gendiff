57a10a31fd3e8f30bc92a2d394ef4cb4cf215bcd
Denis Kalagin <deniskalagin@gmail.com>
2023-10-25 18:45:46 +0600

    update MakeParsingData.js

--- src/MakeParsingData.js
+++ src/MakeParsingData.js
@@ -11,8 +11,19 @@ const compareObjects = (obj1, obj2) => {
   const keys = _.sortBy(_.union(Object.keys(obj1), Object.keys(obj2)));
 
   const result = keys.map((key) => {
+    if (!_.has(obj2, key)) {
+      return formatRemovedKey(key, obj1);
+    }
+    if (!_.has(obj1, key)) {
+      return formatAddedKey(key, obj2);
+    }
+    if (obj1[key] !== obj2[key]) {
+      return formatModifiedKey(key, obj1, obj2);
+    }
+    return formatUnchangedKey(key, obj1);
+  }).join('\n');
 
-  });
+  return `{\n${result}\n}`;
 };
 
 const makeParsingData = (filepath1, filepath2) => {
