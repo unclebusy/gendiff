8093df30d3f89ff234e4c000c8695f9d5779c482
Denis Kalagin <deniskalagin@gmail.com>
2023-10-31 19:19:48 +0600

    modified: gendiff.test.js, package-lock.json, package.json, parsers.js

--- src/parsers.js
+++ src/parsers.js
@@ -3,18 +3,18 @@ import path from 'path';
 import yaml from 'js-yaml';
 
 const parseData = (filepath) => {
-    const absolutePath = path.resolve(process.cwd(), filepath);
-    const data = fs.readFileSync(absolutePath, 'utf-8');
+  const absolutePath = path.resolve(process.cwd(), filepath);
+  const data = fs.readFileSync(absolutePath, 'utf-8');
 
-    if (filepath.endsWith('.json')) {
-        return JSON.parse(data);
-    }
+  if (filepath.endsWith('.json')) {
+    return JSON.parse(data);
+  }
 
-    if (filepath.endsWith('.yml') || filepath.endsWith('.yaml')) {
-        return yaml.load(data);
-    }
+  if (filepath.endsWith('.yml') || filepath.endsWith('.yaml')) {
+    return yaml.load(data);
+  }
 
-    throw new Error(`Unsupported file format: ${filepath}`);
+  throw new Error(`Unsupported file format: ${filepath}`);
 };
 
 export default parseData;
