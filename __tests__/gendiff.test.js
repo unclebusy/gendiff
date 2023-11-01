934f046940e24577df38cea61b3049c1b7610136
Denis Kalagin <deniskalagin@gmail.com>
2023-11-01 18:50:13 +0600

    modedied gendiff.test.js and makeparsingData.js

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -10,12 +10,48 @@ test('compare flat JSON files', () => {
   const filepath2 = path.resolve(__dirname, '__fixtures__/file2.json');
 
   const expected = `{
-  - follow: false
-    host: hexlet.io
-  - proxy: 123.234.53.22
-  - timeout: 50
-  + timeout: 20
-  + verbose: true
+    common: {
+      + follow: false
+        setting1: Value 1
+      - setting2: 200
+      - setting3: true
+      + setting3: null
+      + setting4: blah blah
+      + setting5: {
+            key5: value5
+        }
+        setting6: {
+            doge: {
+              - wow: 
+              + wow: so much
+            }
+            key: value
+          + ops: vops
+        }
+    }
+    group1: {
+      - baz: bas
+      + baz: bars
+        foo: bar
+      - nest: {
+            key: value
+        }
+      + nest: str
+    }
+  - group2: {
+        abc: 12345
+        deep: {
+            id: 45
+        }
+    }
+  + group3: {
+        deep: {
+            id: {
+                number: 45
+            }
+        }
+        fee: 100500
+    }
 }`;
 
   expect(makeParsingData(filepath1, filepath2)).toBe(expected);
@@ -26,12 +62,48 @@ test('compare flat YAML files', () => {
   const filepath2 = path.resolve(__dirname, '__fixtures__/file2.yml');
 
   const expected = `{
-  - follow: false
-    host: hexlet.io
-  - proxy: 123.234.53.22
-  - timeout: 50
-  + timeout: 20
-  + verbose: true
+    common: {
+      + follow: false
+        setting1: Value 1
+      - setting2: 200
+      - setting3: true
+      + setting3: null
+      + setting4: blah blah
+      + setting5: {
+            key5: value5
+        }
+        setting6: {
+            doge: {
+              - wow: 
+              + wow: so much
+            }
+            key: value
+          + ops: vops
+        }
+    }
+    group1: {
+      - baz: bas
+      + baz: bars
+        foo: bar
+      - nest: {
+            key: value
+        }
+      + nest: str
+    }
+  - group2: {
+        abc: 12345
+        deep: {
+            id: 45
+        }
+    }
+  + group3: {
+        deep: {
+            id: {
+                number: 45
+            }
+        }
+        fee: 100500
+    }
 }`;
 
   expect(makeParsingData(filepath1, filepath2)).toBe(expected);
--- src/makeParsingData.js
+++ src/makeParsingData.js
@@ -1,28 +1,40 @@
 import _ from 'lodash';
 import parseData from './parsers.js';
 
-const formatRemovedKey = (key, obj) => `  - ${key}: ${obj[key]}`;
-const formatAddedKey = (key, obj) => `  + ${key}: ${obj[key]}`;
-const formatModifiedKey = (key, obj1, obj2) => `  - ${key}: ${obj1[key]}\n  + ${key}: ${obj2[key]}`;
-const formatUnchangedKey = (key, obj) => `    ${key}: ${obj[key]}`;
+const formatRemovedKey = (key, obj, indent) => `${indent}- ${key}: ${formatValue(obj[key], indent)}`;
+const formatAddedKey = (key, obj, indent) => `${indent}+ ${key}: ${formatValue(obj[key], indent)}`;
+const formatModifiedKey = (key, obj1, obj2, indent) => `${indent}- ${key}: ${formatValue(obj1[key], indent)}\n${indent}+ ${key}: ${formatValue(obj2[key], indent)}`;
+const formatUnchangedKey = (key, obj, indent) => `${indent}  ${key}: ${formatValue(obj[key], indent)}`;
 
-const compareObjects = (obj1, obj2) => {
+const formatValue = (value, indent) => {
+  if (_.isObject(value)) {
+    const keys = Object.keys(value);
+    const result = keys.map((key) => `${indent}    ${key}: ${formatValue(value[key], `${indent}    `)}`).join('\n');
+    return `{\n${result}\n${indent}  }`;
+  }
+  return value;
+};
+
+const compareObjects = (obj1, obj2, indent = '') => {
   const keys = _.sortBy(_.union(Object.keys(obj1), Object.keys(obj2)));
 
   const result = keys.map((key) => {
     if (!_.has(obj2, key)) {
-      return formatRemovedKey(key, obj1);
+      return formatRemovedKey(key, obj1, indent);
     }
     if (!_.has(obj1, key)) {
-      return formatAddedKey(key, obj2);
+      return formatAddedKey(key, obj2, indent);
+    }
+    if (_.isObject(obj1[key]) && _.isObject(obj2[key])) {
+      return `${indent}  ${key}: ${compareObjects(obj1[key], obj2[key], `${indent}    `)}`;
     }
     if (obj1[key] !== obj2[key]) {
-      return formatModifiedKey(key, obj1, obj2);
+      return formatModifiedKey(key, obj1, obj2, indent);
     }
-    return formatUnchangedKey(key, obj1);
+    return formatUnchangedKey(key, obj1, indent);
   }).join('\n');
 
-  return `{\n${result}\n}`;
+  return `{\n${result}\n${indent}}`;
 };
 
 const makeParsingData = (filepath1, filepath2) => {
