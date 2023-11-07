60445e09df9ee360e969560b70b8fc9a9c74803e
Denis Kalagin <deniskalagin@gmail.com>
2023-11-07 17:35:02 +0600

    modified stylish.js

--- src/stylish.js
+++ src/stylish.js
@@ -2,48 +2,43 @@ import _ from 'lodash';
 
 const calculateIndent = (depth, spacesCount = 4) => {
     const count = spacesCount * depth - 2;
-    return count >= 0 ? ' '.repeat(count) : '';
+    return count >= 0 ? ' '.repeat(count) : ' ';
 };
 
-const stringify = (value, replacer = ' ', spacesCount = 4, depth = 1) => {
-    const currentIndent = calculateIndent(depth, spacesCount);
+const stringify = (value, depth = 1) => {
+    const currentIndent = calculateIndent(depth);
 
     if (_.isObject(value)) {
-        const bracketIndent = calculateIndent(depth - 1, spacesCount);
-        const lines = Object.entries(value).map(([key, val]) => `${currentIndent}${key}: ${stringify(val, replacer, spacesCount, depth + 1)}`);
+        const bracketIndent = calculateIndent(depth - 1);
+        const lines = Object.entries(value).map(([key, val]) => `${currentIndent}${key}: ${stringify(val, depth + 1)}`);
         return `{\n${lines.join('\n')}\n${bracketIndent}}`;
     }
 
     return String(value);
 };
 
-const formatNode = (node, indent, spacesCount) => {
-    const formatValue = (value, depth) => {
-        return _.isObject(value) ? stringify(value, ' ', spacesCount, depth) : String(value);
-    };
-
-    const currentIndent = calculateIndent(node.depth, spacesCount);
-    const bracketIndent = calculateIndent(node.depth - 1, spacesCount);
+const formatNode = (node, depth = 1) => {
+    const indent = calculateIndent(depth);
 
     switch (node.type) {
         case 'deleted':
-            return `${indent}- ${node.key}: ${formatValue(node.value, node.depth)}`;
+            return `${indent}- ${node.key}: ${stringify(node.value, depth)}`;
         case 'added':
-            return `${indent}+ ${node.key}: ${formatValue(node.value, node.depth)}`;
+            return `${indent}+ ${node.key}: ${stringify(node.value, depth)}`;
         case 'modified':
-            const oldValue = formatValue(node.oldValue, node.depth);
-            const newValue = formatValue(node.newValue, node.depth);
+            const oldValue = stringify(node.oldValue, depth);
+            const newValue = stringify(node.newValue, depth);
             return `${indent}- ${node.key}: ${oldValue}\n${indent}+ ${node.key}: ${newValue}`;
         case 'unchanged':
-            return `${indent}  ${node.key}: ${formatValue(node.value, node.depth)}`;
+            return `${indent}  ${node.key}: ${stringify(node.value, depth)}`;
         case 'nested':
-            const children = node.children.map((child) => formatNode(child, `${indent}    `, spacesCount)).join('\n');
+            const children = node.children.map((child) => formatNode(child, depth + 1)).join('\n');
             return `${indent}  ${node.key}: {\n${children}\n${indent}  }`;
     }
 };
 
-const formatTree = (tree, spacesCount = 4) => {
-    const result = tree.map((node) => formatNode(node, '', spacesCount)).join('\n');
+const formatTree = (tree) => {
+    const result = tree.map((node) => formatNode(node, 1)).join('\n');
     return `{\n${result}\n}`;
 };
 
