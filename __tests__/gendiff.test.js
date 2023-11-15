ec899f83e4f16d1415fbeeb20d5c7f5f52cbd9d2
Denis Kalagin <deniskalagin@gmail.com>
2023-11-15 14:39:26 +0600

    modified gendiff.test.js

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -6,55 +6,28 @@ import makeParsingData from '../src/makeParsingData.js';
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8').trim();
 
 describe('makeParsingData', () => {
-  const stylishResult = readFile('stylish_result_test.txt').trim();
-  const plainResult = readFile('plain_result_test.txt').trim();
-  const jsonResult = readFile('json_result_test.json').trim();
+  const stylishResult = readFile('stylish_result_test.txt');
+  const plainResult = readFile('plain_result_test.txt');
+  const jsonResult = readFile('json_result_test.json');
 
-  // Stylish Format Tests
-  test('compare file1.json and file2.json in stylish format', () => {
-    const filepath1 = getFixturePath('file1.json');
-    const filepath2 = getFixturePath('file2.json');
-    const result = makeParsingData(filepath1, filepath2, 'stylish').trim();
-    expect(result).toEqual(stylishResult);
-  });
+  test.each([
+    ['file1.json', 'file2.json', 'stylish', stylishResult],
+    ['file1.json', 'file2.json', 'plain', plainResult],
+    ['file1.json', 'file2.json', 'json', jsonResult],
+    ['file1.yml', 'file2.yml', 'stylish', stylishResult],
+    ['file1.yml', 'file2.yml', 'plain', plainResult],
+    ['file1.yml', 'file2.yml', 'json', jsonResult],
+  ])('compare %s and %s in %s format', (file1, file2, format, expected) => {
+    const filepath1 = getFixturePath(file1);
+    const filepath2 = getFixturePath(file2);
+    let result = makeParsingData(filepath1, filepath2, format);
 
-  test('compare file1.yml and file2.yml in stylish format', () => {
-    const filepath1 = getFixturePath('file1.yml');
-    const filepath2 = getFixturePath('file2.yml');
-    const result = makeParsingData(filepath1, filepath2, 'stylish').trim();
-    expect(result).toEqual(stylishResult);
-  });
-
-  // Plain Format Tests
-  test('compare file1.json and file2.json in plain format', () => {
-    const filepath1 = getFixturePath('file1.json');
-    const filepath2 = getFixturePath('file2.json');
-    const result = makeParsingData(filepath1, filepath2, 'plain').trim();
-    expect(result).toEqual(plainResult);
-  });
-
-  test('compare file1.yml and file2.yml in plain format', () => {
-    const filepath1 = getFixturePath('file1.yml');
-    const filepath2 = getFixturePath('file2.yml');
-    const result = makeParsingData(filepath1, filepath2, 'plain').trim();
-    expect(result).toEqual(plainResult);
-  });
-
-  // JSON Format Tests
-  test('compare file1.json and file2.json in json format', () => {
-    const filepath1 = getFixturePath('file1.json');
-    const filepath2 = getFixturePath('file2.json');
-    const result = makeParsingData(filepath1, filepath2, 'json').trim();
-    expect(result).toEqual(jsonResult);
-  });
-
-  test('compare file1.yml and file2.yml in json format', () => {
-    const filepath1 = getFixturePath('file1.yml');
-    const filepath2 = getFixturePath('file2.yml');
-    const result = makeParsingData(filepath1, filepath2, 'json').trim();
-    expect(result).toEqual(jsonResult);
+    if (result.endsWith('\n')) {
+      result = result.slice(0, -1);
+    }
+    expect(result).toEqual(expected);
   });
 });
