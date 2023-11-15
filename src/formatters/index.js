937e5ae4336a4c176c447a28512a418c61ae127f
Denis Kalagin <deniskalagin@gmail.com>
2023-11-15 19:29:09 +0600

    modified makeGenDiff, add formaters\index.js

---
+++ src/formatters/index.js
@@ -0,0 +1,13 @@
+import formatPlain from './plain.js';
+import formatJson from './json.js';
+import formatTree from './stylish.js';
+
+const formatters = {
+    plain: formatPlain,
+    json: formatJson,
+    stylish: formatTree,
+};
+
+const getFormatter = (format) => formatters[format];
+
+export default getFormatter;
--- src/makeGenDiff.js
+++ src/makeGenDiff.js
@@ -1,22 +1,15 @@
 import buildTree from './treeBuilder.js';
 import parseData from './parsers.js';
-import formatTree from './formatters/stylish.js';
-import formatPlain from './formatters/plain.js';
-import formatJson from './formatters/json.js';
+import getFormatter from './formatters/index.js';
 
 const makeGenDiff = (filepath1, filepath2, format) => {
   const data1 = parseData(filepath1);
   const data2 = parseData(filepath2);
 
   const diffTree = buildTree(data1, data2);
+  const formatter = getFormatter(format);
 
-  if (format === 'plain') {
-    return formatPlain(diffTree);
-  }
-  if (format === 'json') {
-    return formatJson(diffTree);
-  }
-  return formatTree(diffTree);
+  return formatter(diffTree);
 };
 
 export default makeGenDiff;
