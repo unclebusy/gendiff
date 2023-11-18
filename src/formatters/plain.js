944349ba1dc9607a57e35658004fa3341f6ab849
Denis Kalagin <deniskalagin@gmail.com>
2023-11-18 19:00:14 +0600

    modified plain.js

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,36 +1,34 @@
 import _ from 'lodash';
 
-const formatPlain = (tree) => {
-  const stringifyValue = (value) => {
-    if (_.isObject(value)) {
-      return '[complex value]';
-    }
-    if (_.isString(value)) {
-      return `'${value}'`;
-    }
-    return value;
-  };
-
-  const renderNode = (node, currentPath = '') => {
-    const fullPath = currentPath ? `${currentPath}.${node.key}` : node.key;
-
-    switch (node.type) {
-      case 'added':
-        return `Property '${fullPath}' was added with value: ${stringifyValue(node.value)}\n`;
-      case 'deleted':
-        return `Property '${fullPath}' was removed\n`;
-      case 'modified':
-        return `Property '${fullPath}' was updated. From ${stringifyValue(node.oldValue)} to ${stringifyValue(node.newValue)}\n`;
-      case 'nested':
-        return node.children.map((child) => renderNode(child, fullPath)).join('');
-      case 'unchanged':
-        return '';
-      default:
-        throw new Error(`Unknown node type: ${node.type}`);
-    }
-  };
-
-  return tree.map((node) => renderNode(node)).join('').trim();
+const stringifyValue = (value) => {
+  if (_.isObject(value)) {
+    return '[complex value]';
+  }
+  if (_.isString(value)) {
+    return `'${value}'`;
+  }
+  return value;
 };
+
+const renderNode = (node, currentPath = '') => {
+  const fullPath = currentPath ? `${currentPath}.${node.key}` : node.key;
+
+  switch (node.type) {
+    case 'added':
+      return `Property '${fullPath}' was added with value: ${stringifyValue(node.value)}\n`;
+    case 'deleted':
+      return `Property '${fullPath}' was removed\n`;
+    case 'modified':
+      return `Property '${fullPath}' was updated. From ${stringifyValue(node.oldValue)} to ${stringifyValue(node.newValue)}\n`;
+    case 'nested':
+      return node.children.map((child) => renderNode(child, fullPath)).join('');
+    case 'unchanged':
+      return '';
+    default:
+      throw new Error(`Unknown node type: ${node.type}`);
+  }
+};
+
+const formatPlain = (tree) => tree.map((node) => renderNode(node)).join('').trim();
 
 export default formatPlain;
